cf09c976ed24b372f688d7f55094b0d21e3f07fa
saimdenizertunc <saimdenizertunc@gmail.com>
2022-07-14 18:46:01 +0300

    Add validation

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -29,118 +29,80 @@ function Signup() {
     
     <form onSubmit={formik.handleSubmit}>
     <label htmlFor="fullName">Full Name</label>
-
        <input
-
          id="fullName"
-
          name="fullName"
-
          type="text"
-
          onChange={formik.handleChange}
-
          value={formik.values.fullName}
-
        />
+    {formik.errors.fullName && formik.touched.fullName && (<div className='error'>Name is a required</div>)}
     
      <label htmlFor="email">Email adress</label>
 
        <input
-
          id="email"
-
          name="email"
-
          type="e-mail"
-
          onChange={formik.handleChange}
-
          value={formik.values.email}
-
        />
     
-    <label htmlFor="email">Phone number</label>
+    {formik.errors.email && formik.touched.email && (<div className='error'>Email must be valid</div>)}
+    
+    <label htmlFor="phoneNumber">Phone number</label>
 
        <input
-
          id="countryCode"
-
          name="countryCode"
-
-
          onChange={formik.handleChange}
-
          value={formik.values.countryCode}
-
        />
-
        <input
-
          id="phoneNumber"
-
          name="phoneNumber"
-
          onChange={formik.handleChange}
-
          value={formik.values.phoneNumber}
-
        />
 
        <label htmlFor="password">Create Password</label>
 
        <input
-
          id="password"
-
          name="password"
-
          type="password"
-
          onChange={formik.handleChange}
-
          value={formik.values.password}
-
        />
+       {formik.errors.password && formik.touched.password && (<div className='error'>Password must contain 5 character</div>)}
 
        <label htmlFor="confirmPassword">Confirm Password</label>
 
        <input
-
          id="confirmPassword"
-
          name="confirmPassword"
-
          type="password"
-
-
          onChange={formik.handleChange}
-
          value={formik.values.confirmPassword}
-
        />
+
+        {formik.errors.confirmPassword && formik.touched.confirmPassword && (<div className='error'>Password should match</div>)}
 
        <label htmlFor="location">Location</label>
 
        <input
-
          id="location"
-
          name="location"
-
          onChange={formik.handleChange}
-
          value={formik.values.location}
-
        />
-    
-
-        
+         
     <br/>
     <button type="submit">Sign Up</button>
     </form>
     
     <p>Already a user? <a href='#'>Login</a></p>
+    
   </div>
   )
 }
--- src/components/validation.js
+++ src/components/validation.js
@@ -1,6 +1,7 @@
 import * as yup from 'yup';
 
 const validations = yup.object().shape({
+  fullName: yup.string().required(),
   email: yup.string().email().required(),
   password: yup.string().min(5).required(),
   confirmPassword: yup.string().oneOf([yup.ref('password')]).required()
